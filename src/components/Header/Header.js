368ba97e2822028ace3eb736e4921206c6e7f4d0
Ryan Williams <ryanjacobwilliams@gmail.com>
2019-03-07 13:59:20 -0600

    Header comp created w/ basic layout

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h2>What?</h2>
+      <h1>ipaddressinfo.com</h1>
+      <h2>clear</h2>
+    </header>
+  );
+};
+
+export default Header;
